085b3a99fa424f606e2db9915ced5ad900e531c7
glikoliz <romaneirih@gmail.com>
2023-07-16 15:15:59 +0700

    Рефакторинг

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from datetime import datetime
 import re
 import requests
 import configparser
+import token_ym
+
 
 def time_to_milliseconds(time_string):
-    match = re.search(r'\[(\d{2}):(\d{2}).(\d{2})\]', time_string)
+    match = re.search(r"\[(\d{2}):(\d{2}).(\d{2})\]", time_string)
     if match:
         minutes, seconds, milliseconds = map(int, match.groups())
         total_milliseconds = (minutes * 60 + seconds) * 1000 + milliseconds
@@ -15,16 +17,17 @@ def time_to_milliseconds(time_string):
     return None
 
 
-def update_presence(track):
+def update_presence(track): #Обновляет статус активности
     dstart = time.time()  # начало трека в мс
     dsend = track["duration_ms"] / 1000 + time.time()  # конец трека в мс
+
     RPC.update(
         large_image=track.get_cover_url(),
         large_text="Чё смотришь",
         small_image="ym",
         small_text="ЯнДеКс МуЗыКа",
         details=track.artists_name()[0],
-        state=track["title"],
+        state=track.title,
         buttons=[{"label": "Кнопка", "url": "https://s.anizam.ru/l/bIY"}],
         start=dstart,
         end=dsend,
@@ -33,36 +36,55 @@ def update_presence(track):
         f"Новый трек: {track.artists_name()[0]} - {track['title']} // {str(datetime.now()).split('.')[0]}"
     )
 
-config = configparser.ConfigParser()
-config.read('conf.ini')
-RPC = Presence(config.get('DSPresence', 'key'))
-RPC.connect()
-client = Client(config.get('MusicClient', 'key')).init()
-prev_track = None
-headers = {
-    'Authorization': config.get('DSToken', 'key'),
-}
-data = {
-    'custom_status': {
-        'text': "",
-        'emoji_id': None,
-        'emoji_name': None
+
+def init(): #читает все токены
+    global headers, client, RPC
+    # читает токен ЯМ из конфига(либо получает его при отсутствии)
+    config = configparser.ConfigParser()
+    config.read("conf.ini")
+    if len(config.get("MusicClient", "key")) <= 5:
+        print("Ключа нема")
+        config.set("MusicClient", "key", token_ym.get_token())
+        with open("conf.ini", "w") as configfile:
+            config.write(configfile)
+    RPC = Presence(config.get("DSPresence", "key"))
+    RPC.connect()
+    client = Client(config.get("MusicClient", "key")).init()
+    headers = {
+        "Authorization": config.get("DSToken", "key"),
     }
-}
+
+
+def update_status(text):
+    requests.patch(
+        "https://discord.com/api/v9/users/@me/settings",
+        headers=headers,
+        json={"custom_status": {"text": text}},
+    )
+
+
+def print_err(error):
+    print(f"Ошибка: {str(error)} // {datetime.now()}")
+
+
+init()
+prev_track = None
 i = 0
 text = False
-start = time.time()
+isError = False
 while True:
     try:
-        last_track = client.queue(client.queues_list()[0].id).get_current_track().fetch_track()  # получает текущий трек
+        last_track = (
+            client.queue(client.queues_list()[0].id).get_current_track().fetch_track()
+        )  # получает текущий трек
         cover = last_track.get_cover_url()  # ссылка на обложку текущего трека
+        # при смене трека
         if prev_track != last_track:
             start = time.time()
             update_presence(last_track)
             try:
-                data['custom_status']['text'] = ""
-                requests.patch('https://discord.com/api/v9/users/@me/settings', headers=headers, json=data)
-                lyrics = last_track.get_lyrics('LRC').fetch_lyrics().split('\n')
+                update_status("")
+                lyrics = last_track.get_lyrics("LRC").fetch_lyrics().split("\n")
                 mil = time_to_milliseconds(lyrics[0])
                 text = True
             except:
@@ -70,19 +92,22 @@ while True:
                 print("У данной песни отсутствует текст")
             prev_track = last_track
             i = 0
+        # выводит текст в описание
         if text:
             now = time.time()
             if (now - start + 1) * 1000 > mil:
                 try:
-                    data['custom_status']['text'] = lyrics[i].split("]")[1].strip()
-                    requests.patch('https://discord.com/api/v9/users/@me/settings', headers=headers, json=data)
+                    update_status(lyrics[i].split("]")[1].strip())
                 except Exception as error:
-                    print(f"{str(error)} // {datetime.now()}")
+                    print_err(error)
                 i += 1
                 try:
                     mil = time_to_milliseconds(lyrics[i])
                 except:
                     text = False
+        isError = False
     except Exception as error:
-        print(f"{str(error)} // {datetime.now()}")
-        time.sleep(5)
+        if isError == False:
+            isError = True
+            print_err(error)
+        # time.sleep(5)
